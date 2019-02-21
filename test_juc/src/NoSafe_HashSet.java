1ce890aeed23082c50ebc79abe5d0939b5e438ec
小乐 <740017278@qq.com>
2019-02-21 08:41:03 +0800

    添加注释

--- test_juc/src/NoSafe_HashSet.java
+++ test_juc/src/NoSafe_HashSet.java
@@ -2,6 +2,10 @@ import java.util.HashSet;
 import java.util.UUID;
 import java.util.concurrent.CopyOnWriteArraySet;
 
+/**
+ * 如果直接使用HashSet会出现ConcurrentModificationExpection异常：并发修改或同时修改异常
+ * HashSet底层就是一个HashMap，key就是保存的值，value是一个Object类型的常量
+ */
 public class NoSafe_HashSet {
     public static void main(String[] args) {
        // HashSet<Object> set = new HashSet<>();
